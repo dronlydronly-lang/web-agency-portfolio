43ad46611943694f6e069f49f06b912e48614e3e
dronlydronly-lang <dronlydronly@gmail.com>
2026-07-28 23:39:31 +0400

    Auto update by Claude

--- app/demo/trend-butik/page.tsx
+++ app/demo/trend-butik/page.tsx
@@ -3,6 +3,7 @@
 import { useMemo, useState } from "react";
 import { DemoShell } from "@/app/_lib/DemoShell";
 import { whatsappUrl } from "@/app/_lib/constants";
+import { ImageWithFallback } from "@/app/_lib/ImageWithFallback";
 import { BagIcon, MinusIcon, PlusIcon, WhatsAppIcon } from "@/app/_lib/icons";
 
 const DELIVERY_FEE = 5;
