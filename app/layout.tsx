45eddbeec761e841a270cb4fa011d3ade8a0410a
dronlydronly-lang <dronlydronly@gmail.com>
2026-07-29 21:28:55 +0400

    Auto update by Claude

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { LanguageProvider } from "./_lib/i18n";
 import "./globals.css";
 
 const geistSans = Geist({
