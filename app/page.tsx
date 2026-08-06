e6254a646713076253ad56182c7f1582405c3e62
dronlydronly-lang <dronlydronly@gmail.com>
2026-08-07 01:24:24 +0400

    Auto update by Claude

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import { FloatingWhatsApp } from "./_lib/FloatingWhatsApp";
 import { HomeClient } from "./_lib/HomeClient";
-import { MeshBackground } from "./_lib/MeshBackground";
 
 const BelowFold = dynamic(() =>
   import("./_lib/BelowFold").then((m) => m.BelowFold)
@@ -9,8 +8,7 @@ const BelowFold = dynamic(() =>
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen w-full flex-col bg-[#030712] text-white">
-      <MeshBackground />
+    <div className="flex min-h-screen w-full flex-col bg-ink text-paper">
       <HomeClient />
       <BelowFold />
       <FloatingWhatsApp />
